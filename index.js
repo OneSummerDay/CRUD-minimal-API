632fb4bbf8ad462175483ac11d3f13fc564fbd01
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-01 21:55:33 +0200

    feat: simulate task creation in POST endpoint

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/', (request, response) => {
+const idCounter = 1;
 
+app.post('/', (request, response) => {
+    const task = {
+        id: idCounter,
+        text: request.body.text,
+        done: false,
+    };
+
+    response.status(201).send(task);
 });
 
 app.get('/', (request, response) => {
