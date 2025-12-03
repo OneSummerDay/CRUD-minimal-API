dd7b36d7480ebe244ba83c91d79e0003be1c032e
OneSummerDay <artem.kraievyi@gmail.com>
2025-12-03 22:26:52 +0200

    feat: connect mock database module to server

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
+const db = require('./db')
 
 const app = express();
 app.use(express.json());
 
-const idCounter = 1;
 
 app.post('/', (request, response) => {
-    const task = {
-        id: idCounter,
-        text: request.body.text,
-        done: false,
-    };
+    
 
     response.status(201).send(task);
 });
